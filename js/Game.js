af23a70e291a2b8cc1c11562c21f8ba771e337de
austin84 <austinveatch84@gmail.com>
2020-05-14 11:25:15 -0700

    createPlayers method

--- js/Game.js
+++ js/Game.js
@@ -1,1 +1,19 @@
-class Game {}
+class Game {
+  constructor() {
+    this.board = new Board();
+    this.players = createPlayers();
+    this.ready = false;
+  }
+
+  /**
+   * Creates two player objects
+   * @return {Array} An array of two player objects.
+   */
+  createPlayers() {
+    let players = [
+      new Player("Player1", 1, "#e15258", true),
+      new Player("Player2", 2, "#e59a13"),
+    ];
+    return players;
+  }
+}
