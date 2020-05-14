5972a4943f5f5b3c8cf3ec83e3e3fe5a7bdbd638
austin84 <austinveatch84@gmail.com>
2020-05-14 13:53:50 -0700

    move left move right

--- js/Token.js
+++ js/Token.js
@@ -3,7 +3,20 @@ class Token {
     this.owner = owner;
     this.id = `token-${index}-${owner.id}`;
     this.dropped = false;
+    this.columnLocation = 0;
   }
+
+  /**
+   * Gets left offset of html element.
+   * @return  {number}   Left offset of token object's htmlToken.
+   */
+  get offsetLeft() {
+    return this.htmlToken.offsetLeft;
+  }
+
+  /**
+   * Draws new HTML token
+   */
 
   drawHTMLToken() {
     const token = document.createElement("div");
@@ -15,5 +28,26 @@ class Token {
 
   get htmlToken() {
     return document.getElementById(this.id);
+  }
+
+  /**
+   * Moves html token one column to left.
+   */
+  moveLeft() {
+    if (this.columnLocation > 0) {
+      this.htmlToken.style.left = this.offsetLeft - 76;
+      this.columnLocation -= 1;
+    }
+  }
+
+  /**
+   * Moves html token one column to right.
+   * @param   {number}    columns - number of columns in the game board
+   */
+  moveRight(columns) {
+    if (this.columnLocation < columns - 1) {
+      this.htmlToken.style.left = this.offsetLeft + 76;
+      this.columnLocation += 1;
+    }
   }
 }
